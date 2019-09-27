cb9bc2d4500bdf7bb5b9cf8888bd70dd4c8eb5c8
rekire <git@reki.re>
2019-09-27 06:35:46 +0200

    Avoid building after updating the meta files

--- release.sh
+++ release.sh
@@ -38,8 +38,8 @@ if [[ $1 == "finish" ]]; then
   git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
   git fetch
   git checkout master
-  git commit -m "Update version number to $VERSION"
+  git commit -m "[skip ci] Update version number to $VERSION"
   git push
-  git tag -fa $TAG -m "[skip travis] Move tag for updated readme"
+  git tag -fa $TAG -m "Move tag for updated readme"
   git push origin master --tags -f
 fi
