cb621ea1458228cc231305b08df08b00a4daa128
rekire <git@reki.re>
2019-09-27 09:18:23 +0200

    Updating logs

--- release-cli.sh
+++ release-cli.sh
@@ -3,12 +3,14 @@ TAG=`git describe --tags`
 MODULE=`echo $TAG|cut -d- -f 1`
 VERSION=`echo $TAG|cut -d- -f 2-`
 
+echo Updating version numbers...
 sed -i -E "s/^version '[^']+'$/version '$VERSION'/g" build.gradle
 git add build.gradle
-echo Updating readme...
+
 sed -i -E "s/version is [0-9][^]\"]+/version is $VERSION/g" readme.md
 sed -i -E "s/cli-[0-9][^ ]+-blue/$TAG-blue/g" readme.md
 git add readme.md
 
+echo Prepairing artifacts...
 # Add link to the not yet created artifact
 ln -s build/libs/cli-$VERSION.jar publish/cli-$VERSION.jar
