1c600a305e99c7fbe7ab3552aa5a254b0f7f9c77
rekire <git@reki.re>
2019-09-27 09:15:30 +0200

    Changing order of execution

--- release.sh
+++ release.sh
@@ -12,25 +12,24 @@ echo $1ing release $VERSION
 if [[ $1 == "update" ]]; then
   mkdir -p publish
   ./release-$MODULE.sh
+fi
 
-  echo Committing changes...
+if [[ $1 == "prepair" ]]; then
+  echo Setup deployment keys...
   # prepare the key
   chmod 600 travis_rsa
   eval `ssh-agent -s`
   ssh-add travis_rsa
 
+  echo Committing changes...
   # Hide the mail address from spam bots
   git config --local user.email "`echo "Z2l0QHJla2kucmU=" | base64 -d`"
   git config --local user.name "rekire"
   git remote set-url origin git@github.com:rekire/ci-test.git
   git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
   git fetch
-fi
-if [[ $1 == "prepair" ]]; then
   echo travis_rsa>>.gitignore
   echo publish>>.gitignore
-fi
-if [[ $1 == "finish" ]]; then
   git checkout master
   git commit -m "[skip ci] Update version number to $VERSION"
   git push
